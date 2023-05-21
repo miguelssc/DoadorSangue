d2a44165d1f4a643947d76fd3513cfc2ab7f4422
miguel <miguelpositivo02@gmail.com>
2023-05-21 17:46:47 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿double idade;
+Console.WriteLine("------doador de Sangue-----");
+Console.WriteLine();
+Console.WriteLine("qual a sua idade?");
+idade =Convert.ToDouble(Console.ReadLine());
+if(idade > 67 || idade< 18){
+Console.WriteLine("voce nao pode ser doador");
+}
+else{
+    Console.WriteLine("voce pode ser um doador");
+}
